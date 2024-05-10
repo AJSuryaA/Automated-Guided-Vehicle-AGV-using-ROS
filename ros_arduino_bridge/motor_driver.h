865a9002ce00e54a9980005d420ba0a682a70613
JAYA SURYA A <52422898+AJSuryaA@users.noreply.github.com>
2024-05-10 19:35:34 +0530

    Update motor_driver.h

--- ros_arduino_bridge/motor_driver.h
+++ ros_arduino_bridge/motor_driver.h
@@ -4,12 +4,12 @@
 
 #ifdef L298_MOTOR_DRIVER
   //below can be changed
-  #define LEFT_MOTOR_ENABLE 11
-  #define LEFT_MOTOR_FORWARD 8
-  #define LEFT_MOTOR_BACKWARD 9
-  #define RIGHT_MOTOR_FORWARD 6
-  #define RIGHT_MOTOR_BACKWARD 7
-  #define RIGHT_MOTOR_ENABLE 10
+  #define LEFT_MOTOR_ENABLE 12
+  #define LEFT_MOTOR_FORWARD 9
+  #define LEFT_MOTOR_BACKWARD 10
+  #define RIGHT_MOTOR_FORWARD 5
+  #define RIGHT_MOTOR_BACKWARD 6
+  #define RIGHT_MOTOR_ENABLE 11
   
 #endif
 
