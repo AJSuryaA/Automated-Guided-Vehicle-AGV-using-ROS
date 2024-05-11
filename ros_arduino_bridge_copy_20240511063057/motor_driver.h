32995f0b8e8549f88f6e9f6b8b7b3ffcfc9a1d2b
JAYA SURYA A <52422898+AJSuryaA@users.noreply.github.com>
2024-05-11 06:31:52 +0530

    Add files via upload

---
+++ ros_arduino_bridge_copy_20240511063057/motor_driver.h
@@ -0,0 +1,18 @@
+/***************************************************************
+   Motor driver function definitions - by Mohit Kumar Gupta
+   *************************************************************/
+
+#ifdef L298_MOTOR_DRIVER
+  //below can be changed
+  #define LEFT_MOTOR_ENABLE 12
+  #define LEFT_MOTOR_FORWARD 9
+  #define LEFT_MOTOR_BACKWARD 10
+  #define RIGHT_MOTOR_FORWARD 5
+  #define RIGHT_MOTOR_BACKWARD 6
+  #define RIGHT_MOTOR_ENABLE 11 
+  
+#endif
+
+void initMotorController();
+void setMotorSpeed(int i, int spd);
+void setMotorSpeeds(int leftSpeed, int rightSpeed);
---
+++ ros_arduino_bridge_copy_20240511063057/motor_driver.ino
@@ -0,0 +1,47 @@
+/***************************************************************
+   Motor driver definitions
+   
+   Add a "#elif defined" block to this file to include support
+   for a particular motor driver.  Then add the appropriate
+   #define near the top of the main ROSArduinoBridge.ino file.
+   
+   *************************************************************/
+
+#ifdef USE_BASE
+   
+#ifdef L298_MOTOR_DRIVER
+  void initMotorController() {
+    digitalWrite(RIGHT_MOTOR_ENABLE, HIGH);
+    digitalWrite(LEFT_MOTOR_ENABLE, HIGH);
+  }
+  
+  void setMotorSpeed(int i, int spd) {
+    unsigned char reverse = 0;
+  
+    if (spd < 0)
+    {
+      spd = -spd;
+      reverse = 1;
+    }
+    if (spd > 255)
+      spd = 255;
+    
+    if (i == LEFT) { 
+      if      (reverse == 0) { analogWrite(LEFT_MOTOR_FORWARD, spd); analogWrite(LEFT_MOTOR_BACKWARD, 0); }
+      else if (reverse == 1) { analogWrite(LEFT_MOTOR_BACKWARD, spd); analogWrite(LEFT_MOTOR_FORWARD, 0); }
+    }
+    else /*if (i == RIGHT) //no need for condition*/ {
+      if      (reverse == 0) { analogWrite(RIGHT_MOTOR_FORWARD, spd); analogWrite(RIGHT_MOTOR_BACKWARD, 0); }
+      else if (reverse == 1) { analogWrite(RIGHT_MOTOR_BACKWARD, spd); analogWrite(RIGHT_MOTOR_FORWARD, 0); }
+    }
+  }
+  
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);
+  }
+#else
+  #error A motor driver must be selected!
+#endif
+
+#endif
